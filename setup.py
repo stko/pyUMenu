c72a05cb058c42e5067de759b1199521eb9b5301
Steffen Köhler <steffen@koehlers.de>
2024-11-26 06:35:44 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name='pyUMenu',                 
+  version='0.1',                 
+  py_modules=["pyUMenu", "pscreen", "ascreen"],   
+  include_package_data=True, 
+  install_requires=[''],        
+)
+
